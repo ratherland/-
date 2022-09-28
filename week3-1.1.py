b8a3c3bc04ae7f52d6262d2610acbbc988a8437c
ratherland <112929867+ratherland@users.noreply.github.com>
2022-09-28 14:41:13 +0800

    week3-1.1.py

---
+++ week3-1.1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+n=1
+a=1
+for n in range(1,100):
+    if n%2==1:
+        print(n,end=' ')
+for n in range(1,50):
+    if n%2==1:
+        a=a*n
+    
+print(a)
+
+
+# In[ ]:
+
+
+
+
