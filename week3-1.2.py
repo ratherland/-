3d912faf0b7ca9180846094ba0f398c7fac8c22f
ratherland <112929867+ratherland@users.noreply.github.com>
2022-09-28 14:42:40 +0800

    week3-1.2.py

---
+++ week3-1.2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#for
+arr = [1,2,3,4,5] 
+reversed_arr = [] 
+for i in reversed(arr): 
+    reversed_arr.append(i) #append在后面加括号里的东西
+print (reversed_arr)
+
+
+# In[7]:
+
+
+#while
+arr = [1,2,3,4,5] 
+i=4
+while i>-1:
+    print (arr[i],end=' ')
+    i=i-1
+
+
+# In[1]:
+
+
+#切片
+arr = [1,2,3,4,5] 
+print(arr[::-1])
+
+
+# In[ ]:
+
+
+
+
